8012ff082c0845d8ed5e5d3f7337fca72e28a218
Kaïn <107337296+plantmama@users.noreply.github.com>
2023-03-27 08:52:31 +0000

    Established what the variable means; created question and one option

---
+++ # THIS IS A PRACTICE RANDOM.py
@@ -0,0 +1,8 @@
+# We will use inout casually
+# Our variable name the user will be realWorld
+
+realWorld= input("string")
+
+if realWorld = "yesOrNo"
+    quit()
+#QUIT is to end game which is sent to the end of code
