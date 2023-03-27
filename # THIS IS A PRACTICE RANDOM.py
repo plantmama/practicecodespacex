665e4d7871df4615e3598401cf175099ecd03a44
Kaïn <107337296+plantmama@users.noreply.github.com>
2023-03-27 19:26:46 +0000

    Prompted first question for active user

--- # THIS IS A PRACTICE RANDOM.py
+++ # THIS IS A PRACTICE RANDOM.py
@@ -1,8 +1,28 @@
+# THIS IS A PRACTICE TEST ASKING BASIC QUESTIONS ABOUT RANDOM STUFF TIL
+# I CAN THINK OF SOMETHING CONSISTANT 
+# THEY WILL BE ONE WORDED ANSWERS
 # We will use inout casually
 # Our variable name the user will be realWorld
 
-realWorld= input("string")
-
-if realWorld = "yesOrNo"
+realWorld= input("To confirm you are human, say yes ")
+# DO NOT FRET, JUST REMEMBER TO ADD A SPACE AFTER EACH QUESTION
+if realWorld != "yes":
     quit()
-#QUIT is to end game which is sent to the end of code
+
+print("Good job human")
+print("This is a lowercase test")
+print("Please answer in lowercase cause I do not want to use the .lower() function")
+
+# Now we use input
+
+input("Who said '' She said she a lesbian, I said Girl me too'' ")
+# drake
+input("In the movie 'Up' which character was a boy scout ")
+
+
+
+
+
+
+
+
